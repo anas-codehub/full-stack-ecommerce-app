9cc97347c1fcbf36cabdbdf7ce60fd83b808fe1a
anas-codehub <talatmahmud002025@gmail.com>
2025-06-12 00:40:30 +0600

    create users table

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-const queryString = process.env.DATABASE as string
+
+const queryString = process.env.DATABASE_URL as string;
 export const connection = postgres(queryString);
 
-export const db = drizzle(connection)
+export const db = drizzle(connection);
